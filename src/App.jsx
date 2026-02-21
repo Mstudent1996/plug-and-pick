c2c5615a33f20b9c341cf4c58f398eeaf21ecbe2
Mstudent1996 <1000041083@mcdmstudent.dk>
2026-02-21 15:22:48 +0100

    ændringer af stier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import Header from "./components/Header/Header.jsx";
+import Header from "./Components/Header/Header.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
 
-import Home from "./pages/Home/Home.jsx";
-import Products from "./pages/Products/Products.jsx";
-import Contact from "./pages/Contact/Contact.jsx";
-import SingleProduct from "./components/SingleProduct/SingleProduct.jsx";
-import CategoryPage from "./pages/CategoryPage/CategoryPage.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Cart from "./pages/Cart/Cart.jsx";
-import Cookies from "./components/Cookies/Cookies.jsx";
-import Terms from "./pages/Documents/Terms.jsx";
-import CookiePolicy from "./pages/Documents/CookiePolicy.jsx";
-import PrivacyPolicy from "./pages/Documents/PrivacyPolicy.jsx";
-import Favorites from "./pages/Favorites/Favorites.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import Products from "./Pages/Products/Products.jsx";
+import Contact from "./Pages/Contact/Contact.jsx";
+import SingleProduct from "./Components/SingleProduct/SingleProduct.jsx";
+import CategoryPage from "./Pages/CategoryPage/CategoryPage.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import Cart from "./Pages/Cart/Cart.jsx";
+import Cookies from "./Components/Cookies/Cookies.jsx";
+import Terms from "./Pages/Documents/Terms.jsx";
+import CookiePolicy from "./Pages/Documents/CookiePolicy.jsx";
+import PrivacyPolicy from "./Pages/Documents/PrivacyPolicy.jsx";
+import Favorites from "./Pages/Favorites/Favorites.jsx";
 
 export default function App() {
   return (
